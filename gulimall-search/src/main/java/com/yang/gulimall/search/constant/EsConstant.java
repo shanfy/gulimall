9e149003dacaae42502f01ee0eeba717dd30b491
shanfy <815481278@qq.com>
2022-12-10 10:00:36 +0800

    append module

--- gulimall-search/src/main/java/com/yang/gulimall/search/constant/EsConstant.java
+++ gulimall-search/src/main/java/com/yang/gulimall/search/constant/EsConstant.java
@@ -6,7 +6,7 @@ package com.yang.gulimall.search.constant;
 public class EsConstant {
 
     //在es中的索引
-    public static final String PRODUCT_INDEX = "gulimall_product";
+    public static final String PRODUCT_INDEX = "gulimall_product_alias";
 
     public static final Integer PRODUCT_PAGESIZE = 16;
 }
